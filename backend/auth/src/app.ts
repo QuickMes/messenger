eda5e15b49e8eee7e1d633ad9c8fb33d86e2bc5a
Slipum <sabirovdanil7@gmail.com>
2025-02-23 18:03:32 +0300

    init

---
+++ backend/auth/src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors'
+import authRoutes from './routes/authRoutes'
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => [
+	console.log(`Auth Service listening on port ${PORT}`)
+])
